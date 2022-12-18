6b3a4f6410a882fc92607a60b33c4b7b8dee9847
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2022-12-18 22:41:18 +0000

    changed home page http response to html layout

--- Finale/Finale/settings.py
+++ Finale/Finale/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Finale.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Finale/FinaleApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
