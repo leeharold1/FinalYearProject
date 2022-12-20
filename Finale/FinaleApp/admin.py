afa2fb68aa3aafa0416d03be777b8a0c4b9f1302
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2022-12-20 19:53:03 +0000

    created superuser(admin) and created order,delivery and bookings tabled + migated them, changed background colour

--- Finale/FinaleApp/admin.py
+++ Finale/FinaleApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from FinaleApp.models import Booking, Delivery, Order
+
+admin.site.register(Booking)
+admin.site.register(Delivery)
+admin.site.register(Order)
