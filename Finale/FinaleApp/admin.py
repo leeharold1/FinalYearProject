99cff72472ac944ea21049f728f417e842d9bf5d
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-02-21 15:07:59 +0000

    New beginnings

--- Finale/FinaleApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from FinaleApp.models import Booking, Delivery, Order
-
-admin.site.register(Booking)
-admin.site.register(Delivery)
-admin.site.register(Order)
--- Finale/FinaleApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FinaleappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'FinaleApp'
