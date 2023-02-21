153b8860620e02e7046e94f54eaf79a613c23282
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-02-21 18:50:00 +0000

    User login complete. Login page created. Links added to home page.

--- Finale/users/urls.py
+++ Finale/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login_user', views.login_user, name="login"),
 ]
--- Finale/users/views.py
+++ Finale/users/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
 	return render(request, 'home.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username and/or password is incorrect.")
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
+
+
+
+	
