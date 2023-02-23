9b04fffb40a5940404cc2a4a06310e7aa63176a7
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-02-23 14:33:09 +0000

    Added user page and user registration

--- Finale/users/urls.py
+++ Finale/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login_user', views.login_user, name="login"),
     path('logout_user', views.logout_user, name="logout"),
+    path('create_user', views.create_user, name="create_user"),
 ]
--- Finale/users/views.py
+++ Finale/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
 	return render(request, 'home.html', {})
@@ -23,5 +24,18 @@ def logout_user(request):
 	logout(request)
 	return redirect('login')
 
+def create_user(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(request, username=username, password=password)
+			login(request, user)
+			return redirect('home')
+	else: 
+		form = UserCreationForm()
 
+	return render(request, 'authenticate/create_user.html', {'form':form,})
 	
