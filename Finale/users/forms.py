f156cc6098223bd8b1ef69f4f91d34141df5b064
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-02-25 17:14:55 +0000

    Adjusted sign-up form with bootstrap classes

--- Finale/users/forms.py
+++ Finale/users/forms.py
@@ -3,10 +3,17 @@ from django.contrib.auth.models import User
 from django import forms 
 
 class CreateUserForm(UserCreationForm):
-	email = forms.EmailField()
-	first_name = forms.CharField(max_length=75)
-	last_name = forms.CharField(max_length=75)
+	email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+	first_name = forms.CharField(max_length=75, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	last_name = forms.CharField(max_length=75, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
 	class Meta:
 		model = User
 		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+	def __init__(self, *args, **kwargs):
+		super(CreateUserForm, self).__init__(*args, **kwargs)
+
+		self.fields['username'].widget.attrs['class'] = 'form-control'
+		self.fields['password1'].widget.attrs['class'] = 'form-control'
+		self.fields['password2'].widget.attrs['class'] = 'form-control'
