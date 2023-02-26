c65bc2818053479880fce8304ef73601b1794be2
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-02-26 17:49:37 +0000

    added css styling and navbar with if statements to base.html

--- Finale/Finale/settings.py
+++ Finale/Finale/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,7 +106,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "users/static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
