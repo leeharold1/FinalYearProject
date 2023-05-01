764c3613f219fac04641d9385578cfb142f7aafa
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-05-01 14:44:43 +0100

    drink options add to array and can print to console

--- Finale/users/static/script.js
+++ Finale/users/static/script.js
@@ -1,21 +1,28 @@
 console.log("Script connected!");
 
+var selectedOptions = []; // declare the array as a global variable
+
 function check(checkbox, uncheck1, uncheck2) {
   // Uncheck other checkboxes in the same group
   $('input[name="' + checkbox.name + '"]').not(checkbox).prop('checked', false);
-  
+
   // Uncheck checkboxes in the other group
   $('#' + uncheck1 + ',#' + uncheck2).prop('checked', false);
 }
 
-function combineOptions() {
+function combineDrinkOptions() {
   var options1 = $('input[name="group1"]:checked').val();
   var options2 = $('input[name="group2"]:checked').val();
-  
+
   if (options1 && options2) {
-    console.log('Selected options: ' + options1 + ', ' + options2);
+    var combinedOptions = options1 + '(' + options2 + ') ';
+    selectedOptions.push(combinedOptions);
+    console.log('Selected options: ' + combinedOptions);
   } else {
     console.log('Please select one option from each group.');
   }
 }
 
+function printDrinkOptions() {
+  console.log('Selected options: ' + selectedOptions);
+}
