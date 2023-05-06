c6ed215a3964d2a303c8fccb59b3d1a4ac8766c3
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-05-06 14:14:20 +0100

    delivery table added

--- Finale/users/admin.py
+++ Finale/users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from .models import Reservations
+from .models import DeliveryOrder
 
 # Register your models here.
 
 admin.site.register(Reservations)
+admin.site.register(DeliveryOrder)
--- Finale/users/models.py
+++ Finale/users/models.py
@@ -1,13 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Reservations(models.Model):
-	name = models.CharField(max_length=50)
-	date = models.DateField()
-	time = models.TimeField()
-	party_size = models.IntegerField()
+    name = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+    party_size = models.IntegerField()
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
+class DeliveryOrder(models.Model):
+    deliveryID = models.IntegerField(primary_key=True)
+    customerID = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    order = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
