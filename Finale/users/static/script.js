5838cbbc2281000c4b2221b45ab4a21e78cd7703
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-05-08 20:00:17 +0100

    2nd loop test

--- Finale/users/static/script.js
+++ Finale/users/static/script.js
@@ -36,6 +36,39 @@ function printDrinkOptions() {
   console.log('Selected options: ' + selectedOptions);
 }
 
+// function printDetails() {
+//   console.log(userID.innerText + ' ' + selectedOptions);
+// }
+
+// function printDetails() {
+//   var totalPrice = 0
+//   var prices = selectedOptions.toString();
+//   prices = prices.replace('Coke(330ml)', 1);
+//   prices = prices.replace('7Up(500ml)', 1.75);
+//   prices = prices.replace('Water(1L)', 2.50);
+//   var convertedOptions = prices.split(",");
+//   totalPrice = convertedOptions.join(", ");
+//   console.log(totalPrice);
+// }
+
 function printDetails() {
-  console.log(userID.innerText + ' ' + selectedOptions);
+  var totalPrice = 0
+  var prices = selectedOptions.toString();
+  for (let i = 0; i < selectedOptions.length; i++) {
+    prices = prices.replace('Coke(330ml)', 1);
+    prices = prices.replace('Coke(500ml)', 2);
+    prices = prices.replace('Coke(1L)', 3);
+    prices = prices.replace('7Up(330ml)', 1);
+    prices = prices.replace('7Up(500ml)', 2);
+    prices = prices.replace('7Up(1L)', 3);
+    prices = prices.replace('Water(330ml)', 1);
+    prices = prices.replace('Water(500ml)', 1.50);
+    prices = prices.replace('Water(1L)', 2);
+  }
+  var convertedOptions = prices.split(",");
+  totalPrice = convertedOptions.join(", ");
+  for (let i = 0; i < totalPrice.length; i++) {
+    totalPrice += totalPrice[i];
+  }
+  console.log(totalPrice);
 }
