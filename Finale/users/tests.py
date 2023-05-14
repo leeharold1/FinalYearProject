d62b3974f62d5ff9557a9f676bb4c7478ab592e3
leeharold1 <56544191+leeharold1@users.noreply.github.com>
2023-05-14 17:27:54 +0100

    added Collection test to tests.py. Successful and added and check notes column for both tables

--- Finale/users/tests.py
+++ Finale/users/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.utils import timezone
-from .models import Reservations, DeliveryOrder
+from .models import Reservations, DeliveryOrder, CollectionOrder
 
 # Create your tests here.
 
@@ -47,3 +47,43 @@ class DeliveryOrderModelTestCase(TestCase):
     def test_delivery_order_price(self):
         # Test if the price of the delivery order is set correctly
         self.assertEqual(self.delivery_order.price, 13.80)
+
+    def test_delivery_order_notes(self):
+        # Test if the notes field of the delivery order is set correctly
+        self.assertEqual(self.delivery_order.notes, 'Extra cheese')
+
+class CollectionOrderModelTestCase(TestCase):
+    def setUp(self):
+        # Create a sample user and collection order for testing
+        self.user = User.objects.create(username='testuser')
+        self.collection_order = CollectionOrder.objects.create(
+            customer=self.user,
+            order='7Up(500ml) ,Chicken Royale(Large) ,Chicken Tenders(Medium)',
+            price=13.75,
+            notes='Plain. No toppings.',
+            pickupTime=timezone.now().time()
+        )
+
+    def test_collection_order_primary_key(self):
+        # Test if the primary key (orderID) of the collection order is not None
+        self.assertIsNotNone(self.collection_order.orderID)
+
+    def test_collection_order_customer(self):
+        # Test if the customer of the collection order is the same as the created user
+        self.assertEqual(self.collection_order.customer, self.user)
+
+    def test_collection_order_order(self):
+        # Test if the order of the collection order is set correctly
+        self.assertEqual(self.collection_order.order, '7Up(500ml) ,Chicken Royale(Large) ,Chicken Tenders(Medium)')
+
+    def test_collection_order_price(self):
+        # Test if the price of the collection order is set correctly
+        self.assertEqual(self.collection_order.price, 13.75)
+
+    def test_collection_order_notes(self):
+        # Test if the notes field of the collection order is set correctly
+        self.assertEqual(self.collection_order.notes, 'Plain. No toppings.')
+
+    def test_collection_order_pickup_time(self):
+        # Test if the pickup time of the collection order is not None
+        self.assertIsNotNone(self.collection_order.pickupTime)
